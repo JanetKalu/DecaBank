1d7d504e8dade162d71654dc6feaa7689fff1245
Janet Kalu <janetkalu82@gmail.com>
2023-02-27 11:40:17 +0100

    basic permission of endpoints commit

---
+++ src/main/java/com/fintech/decabank/DecaBankApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.decabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintech/decabank/DecaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.decabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
