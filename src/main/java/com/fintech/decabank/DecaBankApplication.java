3f281ac7564cb4a7cbd3d7857a231672441d3e3a
Janet Kalu <janetkalu82@gmail.com>
2023-03-08 14:07:37 +0100

    commit for jdbc connectivity

--- src/main/java/com/fintech/decabank/DecaBankApplication.java
+++ src/main/java/com/fintech/decabank/DecaBankApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DecaBankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DecaBankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DecaBankApplication.class, args);
+    }
 
 }
--- src/test/java/com/fintech/decabank/DecaBankApplicationTests.java
+++ src/test/java/com/fintech/decabank/DecaBankApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DecaBankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
